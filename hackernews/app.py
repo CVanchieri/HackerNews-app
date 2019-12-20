4859ac8b468ea645e1e8af23fd6046894f29c8d9
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2019-12-19 20:25:19 -0800

    update

--- hackernews/app.py
+++ hackernews/app.py
@@ -1,23 +1,30 @@
-"""Functions and Routes for the app"""
-# fucntion to create app.
+# imports.
+from .models import DB
+from flask import jsonify
+
+# function for app.
 def create_app():
     app = Flask(__name__)
     # set the DQL to postgress connection.
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///db.sqlite'
     DB.init_app(app)
     
 
-# landing page.
+# landing page route.
     @app.route('/')
     def root():
+        DB.create_all()
 
-    return
-
-
-
+        return ("This will be our 'landing' page, list top 10 saltiest users.")
 
 
+# user search route.
+    @app.route('/user', methods=['POST'])
+    #@app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        #name = name or request.values['by']
+
+        return ("This will be a 'user' page , lists their comments and saltiness.")
 
 
 
